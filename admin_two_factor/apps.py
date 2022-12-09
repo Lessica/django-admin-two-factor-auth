44de655191dfdd2f8e064169d7a1225d0dbaee2e
82Flex <82flex@gmail.com>
2022-12-10 02:39:15 +0800

    auto add 2fa entity when login

--- admin_two_factor/apps.py
+++ admin_two_factor/apps.py
@@ -2,6 +2,17 @@ from django.apps import AppConfig
 from django.utils.translation import gettext_lazy as _
 
 
+def user_logged_in_callback(sender, **kwargs):
+    user = kwargs.get('user', None)
+    if user is not None:
+        from admin_two_factor.models import TwoFactorAuthentication
+        TwoFactorAuthentication.objects.get_or_create(user=user)
+
+
 class TwoFactorAuthenticationConfig(AppConfig):
     name = 'admin_two_factor'
     verbose_name = _('two factor')
+
+    def ready(self):
+        from django.contrib.auth.signals import user_logged_in
+        user_logged_in.connect(user_logged_in_callback)
